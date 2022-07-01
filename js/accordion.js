1da45242d14e34cbbdb3affb43329859ca01b7e6
Vadim Morvanyuk <vadim.morvanyuk@gmail.com>
2022-07-01 20:56:40 +0300

    add block7

--- js/accordion.js
+++ js/accordion.js
@@ -12,3 +12,14 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+
+const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+const INPUT = document.querySelector('input');
+function validateEmail(value) {
+  return EMAIL_REGEXP.test(value);
+}
+function updateInput() {
+  if (validateEmail(INPUT.value)) INPUT.style.borderColor = 'green';
+  else INPUT.style.borderColor = 'red';
+}
+INPUT.addEventListener('input', updateInput);
