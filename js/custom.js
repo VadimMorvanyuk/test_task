73e34032aa757e8d5dd2a98e5cda61933cd694ea
Vadim Morvanyuk <vadim.morvanyuk@gmail.com>
2022-07-01 19:17:16 +0300

    add block5

--- js/custom.js
+++ js/custom.js
@@ -15,7 +15,7 @@ jQuery(document).ready(function () {
 		nav: false,
 		navText: ['<i class="fa fa-regular fa-chevron-left"></i>', '<i class="fa fa-regular fa-chevron-right"></i>'],
 		dots: false,
-		mouseDrag: true,
+		mouseDrag: false,
 		margin: 30,
 		stagePadding: 0,
 		autoWidth: false,
@@ -54,6 +54,64 @@ jQuery(document).ready(function () {
 				items: 2,
 				dots: true,
 				mouseDrag: true,
+				nav: true,
+				autoplay: false
+			},
+			
+		}
+	});
+    let carousel2 = jQuery('#stores');
+
+    carousel2.owlCarousel({
+		items: 2,
+		autoplay: false,
+		autoplayTimeout: 4000,
+		autoplayHoverPause: false,
+		smartSpeed: 1000,
+		dotsSpeed: 1000,
+		loop: true,
+		nav: false,
+		navText: ['<i class="fa fa-regular fa-chevron-left"></i>', '<i class="fa fa-regular fa-chevron-right"></i>'],
+		dots: false,
+		mouseDrag: false,
+		margin: 30,
+		stagePadding: 0,
+		autoWidth: false,
+		responsiveClass: true,
+		responsive:{
+			0:{
+				items: 2,
+				dots: true,
+				nav: false,
+				mouseDrag: false,
+				autoplay: false,
+				smartSpeed: 1000
+			},
+			479:{
+				items: 2,
+				dots: true,
+				mouseDrag: false,
+				autoplay: false,
+				smartSpeed: 1000
+			},
+			767:{
+				items: 2,
+				dots: true,
+				mouseDrag: false,
+				autoplay: false,
+				smartSpeed: 1000
+			},
+			991:{
+				items: 2,
+				dots: true,
+				mouseDrag: false,
+				autoplay: false,
+				smartSpeed: 1000
+			},
+			1099:{
+				items: 2,
+				dots: true,
+				mouseDrag: false,
 				nav: true,
 				autoplay: false
 			},
